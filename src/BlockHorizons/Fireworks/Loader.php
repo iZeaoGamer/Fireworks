5100b0d2d4667463959b28d7b0e7315b9fdc63f0
iZeaoGamer <jameshayward14@icloud.com>
2020-05-14 05:58:31 +0100

    Update Loader.php

--- src/BlockHorizons/Fireworks/Loader.php
+++ src/BlockHorizons/Fireworks/Loader.php
@@ -13,9 +13,11 @@ use pocketmine\item\ItemFactory;
 use pocketmine\plugin\PluginBase;
 
 class Loader extends PluginBase {
-
+public function onLoad(): void{
+ItemFactory::registerItem(new Fireworks());
+}
 	public function onEnable(): void {
-		ItemFactory::registerItem(new Fireworks());
+		
 		Item::initCreativeItems(); //will load firework rockets from pocketmine's resources folder
 		if(!Entity::registerEntity(FireworksRocket::class, false, ["FireworksRocket"])) {
 			$this->getLogger()->error("Failed to register FireworksRocket entity with savename 'FireworksRocket'");
